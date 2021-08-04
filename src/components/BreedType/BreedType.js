ceefcc47273e9aed3977759260b90fd392ef0213
Sydney Bear <sbear3915@gmail.com>
2021-08-04 10:34:40 -0600

    Set up breedtype component

---
+++ src/components/BreedType/BreedType.js
@@ -0,0 +1,10 @@
+import React, { useState}  from 'react';
+import './BreedType.css';
+
+const BreedType = () => {
+  return (
+
+  )
+}
+
+export default BreedType;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,7 +4,7 @@ import './NavBar.css';
 const NavBar = () => {
   return (
     <nav>
-
+      
     </nav>
   )
 }
