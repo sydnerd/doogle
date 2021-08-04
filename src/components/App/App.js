b5945880c592fe928099109983a3d5905e8262ce
Sydney Bear <sbear3915@gmail.com>
2021-08-04 10:22:11 -0600

    Clean up app css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,16 +2,18 @@ import React, { useState}  from 'react';
 import './App.css';
 
 const App = () => {
-
-}
-
   return (
     <div className="App">
       <header className="App-header">
-       
+       <h1>
+         Doogle
+       </h1>
       </header>
     </div>
   );
 }
+
+  
+
 
 export default App;
