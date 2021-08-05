4d2cf2c8931cdb963a2b64082e3f4d7ab92a955e
Sydney Bear <sbear3915@gmail.com>
2021-08-05 08:57:57 -0600

    Add doogle logo

--- src/components/DogCard/DogCard.js
+++ src/components/DogCard/DogCard.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import heart from '../../resources/heart.svg';
+import x from '../../resources/x.svg';
+// import logo from '../../resources/doogle_logo.png';
 import './DogCard.css';
 
 const DogCard = ({randomDog}) => {
   return (
     <div>
-       <h1>Logo will go here</h1>
+       {/* <img src={logo} alt='doogle logo'/> */}
         <img src={randomDog} alt="dog" />
         <div>
-          <span className="material-icons-outlined">favorite</span>
-          <span className="material-icons-outlined">cancel</span>
+          <img className="heart"src={heart}/>
+          <img className="x"src={x}/>
         </div>
         
     </div>
