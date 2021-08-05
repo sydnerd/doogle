49d0c7fc1620818e8b77caf2e2be3191cf0187ff
Sydney Bear <sbear3915@gmail.com>
2021-08-05 08:47:26 -0600

    Add heart and x icons

--- src/components/DogCard/DogCard.js
+++ src/components/DogCard/DogCard.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './DogCard.css';
 
 const DogCard = ({randomDog}) => {
   return (
     <div>
-       <h1>This is where the card will go</h1>
-        <img src={randomDog} />
+       <h1>Logo will go here</h1>
+        <img src={randomDog} alt="dog" />
+        <div>
+          <span className="material-icons-outlined">favorite</span>
+          <span className="material-icons-outlined">cancel</span>
+        </div>
+        
     </div>
    
   )
