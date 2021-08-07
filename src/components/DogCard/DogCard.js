c6ef89e6245b8277cc3b62023518af36745cc497
Sydney Bear <sbear3915@gmail.com>
2021-08-06 18:16:32 -0600

    Align dog image in card to center

--- src/components/DogCard/DogCard.js
+++ src/components/DogCard/DogCard.js
@@ -8,17 +8,15 @@ import './DogCard.css';
 const DogCard = ({dog, addDog, removeDog}) => {
   return (
     <div className='card-container'>
-       {/* <img className='logo-image' src={logo} alt='doogle logo'/> */}
-        <img className='dog-image'src={dog} alt="dog" />
-        <div className='icon-container'>
-          {/* <img className='x icon'src={x} onClick={()=>removeDog(dog)}/> */}
-          <span className='material-icons x icon' onClick={()=>removeDog(dog)}>close</span>
-          {/* <img className='heart icon'src={heart} onClick={() => addDog(dog)}/> */}
-          <span className='material-icons heart icon' onClick={()=>addDog(dog)}>favorite</span>
-
-        </div> 
+      <img className='dog-image'src={dog} alt="dog" />
+      <div className='icon-container'>
+        <span className='material-icons x icon' onClick={()=>removeDog(dog)}>close</span>
+        <span className='material-icons heart icon' onClick={()=>addDog(dog)}>favorite</span>
+      </div> 
     </div>
   )
 }
 
 export default DogCard;
+
+{/* <img className='logo-image' src={logo} alt='doogle logo'/> */}
