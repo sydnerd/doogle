ab12eb701237d590fb8af1c2e49b6501d39e15ca
Sydney Bear <sbear3915@gmail.com>
2021-08-09 10:38:35 -0600

    Add test in nav for active nav links

--- cypress/integration/navBar-spec.js
+++ cypress/integration/navBar-spec.js
@@ -11,7 +11,7 @@ describe('Nav Bar', () => {
       .contains('Find Match')
       .click()
       .url('http://localhos:3000/')
-  })
+  });
 
   it('Should contain a dog card with image, x button and heart button when find match is clicked', () => {
     cy.get('.find-match')
@@ -20,27 +20,33 @@ describe('Nav Bar', () => {
       .get('.dog-image').should('exist').should('be.visible')
       .get('.x').should('exist').should('be.visible')
       .get('.heart').should('exist').should('be.visible')
-  })
+  });
 
   it('Should contain a link to a list of all matches', () => {
     cy.get('nav')
       .contains('Your Matches')
       .click()
       .url('http://localhost:3000/matches')
-  })
+  });
 
   it('Should have a title', () => {
     cy.get('header')
       .contains('doogle')
-  })
+  });
 
   it('Should have a tag line', () => {
     cy.get('header')
       .contains('discover your golden')
-  })
+  });
 
   it('Should have an dog logo/image', () => {
     cy.get('header')
       .get('.logo-image').should('exist').should('be.visible')
-  })
+  });
+
+  it('Navigation link styling should reflect the current page', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('.find-match').should('have.class', 'selected-title');
+    cy.get('.your-matches').should('not.have.class', 'selected-title');
+  });
 })
